736716ad18bc2598119c2701b2934da7a9b0880e
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2022-10-07 00:25:30 +0300

    first commit

---
+++ src/test/java/stepDefinitions/AmazonStepDefinition.java
@@ -0,0 +1,22 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import utilities.Driver;
+
+public class AmazonStepDefinition {
+    @Given("Kullanici amazon sayfasina gider")
+    public void kullaniciAmazonSayfasinaGider() {
+        Driver.getDriver().get("https://amazon.com");
+    }
+
+    @Then("Kullanici nutella aratir")
+    public void kullaniciNutellaAratir() {
+        Driver.getDriver().findElement(By.id("twotabsearchtextbox")).sendKeys("Nutella", Keys.ENTER);
+    }
+
+}
---
+++ src/test/resources/features/amazonSearch.feature
@@ -0,0 +1,6 @@
+Feature: Amazon Search
+
+  Scenario: TC01 Kullanici amazonda Nutella aratir
+
+    Given Kullanici amazon sayfasina gider
+    Then Kullanici nutella aratir
