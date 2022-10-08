e6dd1cf89bd9e33536a7c1dd9432a10888e61ecd
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2022-10-08 14:13:18 +0300

    8.10.2022 V1

---
+++ src/test/java/homeworks/Homeworks.java
@@ -0,0 +1,29 @@
+package homeworks;
+
+public class Homeworks {
+
+    // -----1.---------//
+    // - Google'a gidiniz
+    //- trendyol'u aratınız
+    //- trendyol linkini bulup siteye gidiniz
+    //- makas aratınız
+    //- toplam ürün sayısını alınız
+    //- yeni sekmede morhipo'ya gidiniz
+    //- makas aratınız
+    //- toplam ürün sayısını alınız
+    //- iki sitedeki topla makas sayısını karşılaştırınız
+    //- Önce ürün sayısı fazla olan siteyi kapatınız
+    //- Sonra diğer sayfayıda kapatınız
+
+
+    //---------2.-------//
+    // http://tutorialsninja.com/demo/index.php?route=common/home sayfasına gidiniz
+    // Phones & PDAs'a tıklayınız
+    // Telefonların markalarını alınız
+    // Tüm öğeleri sepete ekleyiniz
+    // Sepete tıklayınız
+    // Sepetteki isimleri alınız
+    // Sepetteki ve sayfadaki ürünlerin doğru olduğunu karşılaştırınız
+
+
+}
