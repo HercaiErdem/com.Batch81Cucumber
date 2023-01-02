7a7058123f74f9b720a10c716f1586239343bee0
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-01-02 15:55:06 +0300

    02.01.2022 v1

--- src/test/java/pages/PracticeTestPage.java
+++ src/test/java/pages/PracticeTestPage.java
@@ -1,9 +1,8 @@
 package pages;
 
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-import utilities.Driver;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.*;
+import utilities.*;
 
 import java.util.List;
 
@@ -18,13 +17,14 @@ public class PracticeTestPage {
 
     @FindBy(xpath = "//p[text()=' down 32']")
     public List<WebElement> down32;
-    // //p[.=' down 32']
-    // //*[.=' down 32']
+    //p[.=' down 32']
+    //*[.=' down 32']
+
 
     @FindBy(xpath = " //p[text()='click']")
     public List<WebElement> clickText;
-    // //p[text()='click']
-    // //p[.='click']
-    //  //*[.='click']
+    //p[text()='click']
+    //p[.='click']
+    //*[.='click']
 
 }
