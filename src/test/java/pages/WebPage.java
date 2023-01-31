c9c4ebd2fcc72704a1b281e43b04af07730b3e28
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-01-31 13:55:31 +0300

    v3

--- src/test/java/pages/WebPage.java
+++ src/test/java/pages/WebPage.java
@@ -1,18 +1,19 @@
- package pages;
+package pages;
 
- import org.openqa.selenium.*;
- import org.openqa.selenium.support.*;
- import utilities.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.*;
+import utilities.*;
 
- public class WebPage {
-     public WebPage (){
-         PageFactory.initElements(Driver.getDriver(),this);
+public class WebPage {
+    public WebPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
 
-     }
-     @FindBy (xpath = "(//*[@class='section-title'])[2]")
-     public WebElement login;
-     @FindBy (xpath = "//*[@id='text']")
-     public WebElement userName;
+    }
+
+    @FindBy(xpath = "(//*[@class='section-title'])[2]")
+    public WebElement login;
+    @FindBy(xpath = "//*[@id='text']")
+    public WebElement userName;
 
 
- }
+}
